eaa84f2c40625bd3037a5634ff3a7db8c13e9e7c
JMateu <jmateu@MacBook-Air-de-JMateu.local>
2021-04-20 13:01:35 +0200

    primer commit

---
+++ exemple.py
@@ -0,0 +1,13 @@
+print("Benvinguts i benvingudes a Python")
+
+print("Introdueix un valor")
+numero1=int(input())
+
+
+print("Introdueix un altre valor")
+numero2=int(input())
+
+#aquest part ens permet sumar dos números
+suma=numero1 + numero2
+
+print("La suma es " + str(suma))
